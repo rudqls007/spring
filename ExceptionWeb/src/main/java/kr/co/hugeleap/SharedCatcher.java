1cda120a0a35ea9718169bb2d9871ac9204f74c7
rudqls007 <rudqls007@gmail.com>
2022-10-26 10:12:53 +0900

    Exception

--- ExceptionWeb/src/main/java/kr/co/hugeleap/SharedCatcher.java
+++ ExceptionWeb/src/main/java/kr/co/hugeleap/SharedCatcher.java
@@ -13,8 +13,7 @@ import org.springframework.http.HttpStatus;
 @ControllerAdvice // 모든 컨트롤러에서 발생하는 예외를 다 처리하게 됨 (모든 패키지에 적용)
 public class SharedCatcher {
 
-//	@ExceptionHandler(Exception.class)
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(Exception.class)
 	public String catcher(Exception ex, Model m) { 
 		System.out.println("catcher() in SharedCatcher");
 		System.out.println("m = " + m.getAttribute("msg")); // 다른 모델을 사용하고 있음.
@@ -23,8 +22,7 @@ public class SharedCatcher {
 		return "error";
 	}
 	
-//	@ExceptionHandler({ NullPointerException.class , FileNotFoundException.class })
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler({ NullPointerException.class , FileNotFoundException.class })
 	public String catcher2(Exception ex, Model m) {
 		m.addAttribute("ex", ex);
 		return "error";
