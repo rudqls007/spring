b1f143301b6e0612dec7e0cee3cb46d8819a2047
rudqls007 <rudqls007@gmail.com>
2022-10-27 12:28:23 +0900

    commit

---
+++ SpringDBWeb/src/test/java/kr/co/magic/DBConnectionTest3Test.java
@@ -0,0 +1,19 @@
+package kr.co.magic;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class DBConnectionTest3Test {
+
+	public static void main(String[] args) throws SQLException {
+		
+
+	}
+
+}
