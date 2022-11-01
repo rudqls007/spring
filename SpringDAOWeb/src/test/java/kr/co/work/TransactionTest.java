359dfea44a3d10500ef35c11fa38ae34d02c11ca
rudqls007 <rudqls007@gmail.com>
2022-11-01 14:03:06 +0900

    commit

--- SpringDAOWeb/src/test/java/kr/co/work/TransactionTest.java
+++ SpringDAOWeb/src/test/java/kr/co/work/TransactionTest.java
@@ -3,6 +3,7 @@ package kr.co.work;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Date;
 
 import javax.sql.DataSource;
 
@@ -13,24 +14,46 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
 public class TransactionTest {
 	
 	@Autowired
 	DataSource ds;
 	
 	@Test
-	public void tarnsactionTest() throws SQLException {
+	public void tarnsactionTest() throws Exception  {
 		Connection conn = null;
 		
-		deleteAll();
-		conn = ds.getConnection();
-		conn.setAutoCommit(false);
-		
-		String sql = "INSERT INTO t_user values(?, ?, ?, ?, ?, ?, now())";
-		
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		
+		try {
+			
+			deleteAll();
+			
+			conn = ds.getConnection();
+			conn.setAutoCommit(true);
+			
+			String sql = "INSERT INTO t_user values(?, ?, ?, ?, ?, ?, now())";
+			
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, "ezen1");
+			pstmt.setString(2, "0111");
+			pstmt.setString(3, "ezen1");
+			pstmt.setString(4, "ezen1@gmail.com");
+			pstmt.setDate(5, new java.sql.Date(new Date().getTime()));
+			pstmt.setString(6, "fb");
+			
+			int rowCnt = pstmt.executeUpdate();
+			
+			pstmt.setString(1, "ezen1");
+			
+			rowCnt = pstmt.executeUpdate();
+			
+			conn.commit();
+			
+		} catch (Exception e) {
+			conn.rollback();
+			e.printStackTrace();
+		}
+
 	
 	}
 
