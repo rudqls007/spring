efdbf887209964b55023f586405fa986fcfb6fc4
rudqls007 <rudqls007@gmail.com>
2022-11-02 11:08:55 +0900

    commit

---
+++ SpringDAOWeb/src/main/java/kr/co/work/aop/LoggingAdvice.java
@@ -0,0 +1,10 @@
+package kr.co.work.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class LoggingAdvice {
+
+	public Object methodCallLog(ProceedingJoinPoint pjp) {
+		return null;
+	}
+}
