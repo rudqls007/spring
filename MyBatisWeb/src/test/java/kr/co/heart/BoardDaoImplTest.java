a3623fa6bf9041803a43a5162c7cc2c35c6b1cae
rudqls007 <rudqls007@gmail.com>
2022-11-03 18:27:51 +0900

    commit

--- MyBatisWeb/src/test/java/kr/co/heart/BoardDaoImplTest.java
+++ MyBatisWeb/src/test/java/kr/co/heart/BoardDaoImplTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import kr.co.heart.dao.BoardDao;
+import kr.co.heart.domain.BoardDto;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"} )
@@ -18,8 +19,13 @@ public class BoardDaoImplTest {
 	private BoardDao boardDao;
 	
 	@Test
-	public void selectTest() {
+	public void selectTest() throws Exception {
 		assertTrue(boardDao != null);
 		System.out.println("boardDao = " + boardDao);
+		
+		BoardDto boardDto =  boardDao.select(1);
+		System.out.println("boardDto = " + boardDto );
+		assertTrue(boardDto.getBno().equals(1));
+		
 	}
 }
---
+++ MyBatisWeb/src/test/java/kr/co/heart/pageResolverTest.java
@@ -0,0 +1,43 @@
+package kr.co.heart;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import kr.co.heart.domain.pageResolver;
+
+public class pageResolverTest {
+
+	@Test
+	public void test() {
+		pageResolver pr = new pageResolver(250, 1);
+		pr.print();
+		System.out.println("pr = " + pr);
+		System.out.println();
+		
+		assertTrue(pr.getBeginPage() == 1);
+		assertTrue(pr.getEndPage() == 10);
+	}
+	
+	@Test
+	public void test2() {
+		pageResolver pr = new pageResolver(250, 11);
+		pr.print();
+		System.out.println("pr = " + pr);
+		System.out.println();
+		
+		assertTrue(pr.getBeginPage() == 11);
+		assertTrue(pr.getEndPage() == 20);
+	}
+	
+	@Test
+	public void test3() {
+		pageResolver pr = new pageResolver(255, 25);
+		pr.print();
+		System.out.println("pr = " + pr);
+		System.out.println();
+		
+		assertTrue(pr.getBeginPage() == 21);
+		assertTrue(pr.getEndPage() == 26);
+	}
+}
